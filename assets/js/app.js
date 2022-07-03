161a30d4db6dedda656abcadbea4cf410846c92f
prasanna-dot <prasannavenaksteh20.01.2k1@gmail.com>
2022-07-02 21:28:18 -0800

    Add

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,13 @@
-function chatbot(){
-  document.getElementById("message").style.display ="block";
-}
+$(document).ready(function(){
+    $("img").click(function(){
+    var t = $(this).attr("src");
+    $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+    $("#myModal").modal();
+  });
+  });//EOF Document.ready
 
-function chatbotclase(){
-    document.getElementById("message").style.display ="none";
-  }
+  for (let img = 1; img < 46; img++) {
+    let create = `<img src="./assets/photography/photography_1-min.jpg">`
+    photos = photos + create
+    document.getElementById('gallery').innerHTML = photos
+}
